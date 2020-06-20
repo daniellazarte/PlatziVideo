30dee42bc8bac5bb43c99d6c8590c370df673055
Daniel Lazarte <daniel.lazarte@gmail.com>
2020-06-20 13:57:53 -0500

    FInal deploy Project V1.0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import HelloWorld from './Components/HelloWorld'
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import HelloWorld from './Components/HelloWorld'
+import App from './containers/App';
+import './assets/styles/App.scss';
 
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
